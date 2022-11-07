23778d9fa426728f6f471d059241465282584fec
Aubry <aubrii@hotmail.fr>
2022-11-07 11:36:44 +0100

    CRUD Angular cout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent} from "./header/header.component";
+import {FormCoutComponent} from "./form-cout/form-cout.component";
+import {ListCoutComponent} from "./list-cout/list-cout.component";
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'cout/:id', component: FormCoutComponent},
+  { path: 'cout', component: FormCoutComponent, },
+  {path: '', component: HeaderComponent},
+  {path: 'list', component: ListCoutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HeaderComponent } from './header/header.component';
+import { ListCoutComponent } from './list-cout/list-cout.component';
+import { CoutComponent } from './cout/cout.component';
+import { FormCoutComponent } from './form-cout/form-cout.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HeaderComponent,
+    ListCoutComponent,
+    CoutComponent,
+    FormCoutComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+        MatButtonModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
